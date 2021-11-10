9ec641f020e560ddd8aabae08dda35551c3ce431
Berksu Kısmet <berksukismet@gmail.com>
2021-11-10 23:12:18 +0300

    Initial Commit

---
+++ Shared/movieApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieApp.swift
+//  Shared
+//
+//  Created by Berksu Kısmet on 10.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct movieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
