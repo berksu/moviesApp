9bff4261da96cd9b0dba14740df47c3dd86d9950
Berksu Kısmet <berksukismet@gmail.com>
2021-12-01 23:32:51 +0300

    refactoring

---
+++ Shared/MovieListPage/Tabs/TabBackgroundView.swift
@@ -0,0 +1,65 @@
+//
+//  TabView.swift
+//  movie (iOS)
+//
+//  Created by Berksu Kısmet on 1.12.2021.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct TabBackgroundView: View {
+    @Binding var isSideMenuShow: Bool
+    @State var test = ""
+    //AppStorage and userdefaults tried
+    @AppStorage("tabSelection") private var selection: Int = 0
+    //@State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+    //UserDefaults.standard.set(self.tapCount, forKey: "Tap")
+
+    var body: some View {
+        TabView(selection: $selection){
+            AllMoviesView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }.tag(0)
+            
+            
+            FavouritesView()
+                .tabItem {
+                    Image(systemName: "heart.fill")
+                    Text("Favourites")
+                }
+                .tag(1)
+        }
+
+        // 3 principal
+        .navigationTitle(selection == 0 ? "Top Movies": "Favourite Movies") // bu satır constraint hatası verdiriyor
+        .toolbar{
+//            ToolbarItem(placement: .principal) {
+//                Text(selection == 0 ? "Top \(moviesViewModel.allMovies.count) Movies": "Favourite Movies")
+//            }
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button {
+                    withAnimation(.spring()){
+                        isSideMenuShow.toggle()
+                    }
+                } label: {
+                    KFImage(URL(string: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"))
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(Circle())
+                        .shadow(radius: 20)
+                        .frame(height: 50)
+                        .background(.clear)
+                }
+            }
+        }
+    }
+}
+
+struct TabView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBackgroundView(isSideMenuShow: .constant(false))
+    }
+}
